3d3c84d87e37ce39967eec9d2db403f4d2c7beb4
Giacomo Marciani <giacomo.marciani@gmail.com>
2013-12-02 17:18:15 +0100

    Test

--- robomap/src/robomap/test/Test.java
+++ robomap/src/robomap/test/Test.java
@@ -1,19 +1,27 @@
 package robomap.test;
 
-import robomap.model.base.Location;
+import robomap.model.home.Home;
 import robomap.model.robot.Robot;
+import robomap.model.vector.Location;
 
 public class Test {
 	
-	private static final String XML_PLANIMETRY_PATH = "src/robomap/test/sample_home.xml";
+	private static final String XML_PLANIMETRY_PATH = "test/sample_home.xml";
 
 	public static void main(String[] args) {
 		Robot mRobot = new Robot("mRobot");
-		mRobot.importHomeFromXML(XML_PLANIMETRY_PATH);
-		mRobot.selectHome();
+		Home mHome = mRobot.importHomeFromXML(XML_PLANIMETRY_PATH);
+		mRobot.setHome(mHome);
 		mRobot.goTo(new Location(9,9));
 		mRobot.goTo(new Location(0,9));
 		mRobot.goToStart();
+		
+		Robot yRobot = new Robot("yRobot");
+		Home yHome = mRobot.importHomeFromXML(XML_PLANIMETRY_PATH);
+		yRobot.setHome(yHome);
+		yRobot.goTo(new Location(9,9));
+		yRobot.goTo(new Location(0,9));
+		yRobot.goToStart();
 	}
 
 }
