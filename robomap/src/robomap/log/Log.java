c7fad39a3d124ad0a4ac244b5db63a34353cd088
Giacomo Marciani <giacomo.marciani@gmail.com>
2013-12-02 17:20:30 +0100

    Logger release 1.0

--- robomap/src/robomap/log/Log.java
+++ robomap/src/robomap/log/Log.java
@@ -2,29 +2,17 @@ package robomap.log;
 
 import java.sql.SQLException;
 
-import robomap.model.object.Action;
-import robomap.model.object.Object;
-import robomap.model.robot.Movement;
-
 public class Log {
 	
 	public static final void printSQLException(String className, String methodName, SQLException exc) {
 		int errCode = exc.getErrorCode();
 		String errMessage = exc.getMessage();
-		System.out.println(className + " : " + methodName + " : " + errCode + " : " + errMessage);
+		System.err.println(className + " : " + methodName + " : " + errCode + " : " + errMessage);
 	}
 	
 	public static final void printXMLException(String className, String methodName, Exception exc) {
 		String errMessage = exc.getMessage();
-		System.out.println(className + " : " + methodName + " : " + errMessage);
-	}
-	
-	public static final void printMovement(Movement movement) {
-		System.out.println("#Movement: " + movement.toString());
-	}
-
-	public static void printAction(Object object, Action action) {
-		System.out.println("ACTION " + action.getName() + " ON OBJECT " + object.getName() + " --> " + action.getStatus());
+		System.err.println(className + " : " + methodName + " : " + errMessage);
 	}
 
 }
